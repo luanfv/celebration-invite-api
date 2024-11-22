7c5d6c6afbbdad546062cc8741eafb41df2378d9
luanfv <luanfrancavieira@gmail.com>
2024-11-22 19:44:40 -0300

    test: add unit test

---
+++ src/app.controller.spec.ts
@@ -0,0 +1,13 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { AppModule } from './app.module';
+import { AppController } from './app.controller';
+
+describe('AppController', () => {
+    it('SHOULD be controller defined', async () => {
+        const module = await Test.createTestingModule({
+            imports: [AppModule]
+        }).compile();
+        const controller = module.get(AppController);
+        expect(controller).toBeDefined();
+    });
+});
