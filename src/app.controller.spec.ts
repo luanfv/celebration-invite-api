a4622a57e9d410dce06e8fe901cd36787c75bdf5
luanfv <luanfrancavieira@gmail.com>
2024-11-24 14:43:27 -0300

    doc: config the readme and prettier

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -3,11 +3,11 @@ import { AppModule } from './app.module';
 import { AppController } from './app.controller';
 
 describe('AppController', () => {
-    it('SHOULD be controller defined', async () => {
-        const module = await Test.createTestingModule({
-            imports: [AppModule]
-        }).compile();
-        const controller = module.get(AppController);
-        expect(controller).toBeDefined();
-    });
+  it('SHOULD be controller defined', async () => {
+    const module = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+    const controller = module.get(AppController);
+    expect(controller).toBeDefined();
+  });
 });
